ad8a03646158d274d80b3d35a38cf3c7b18000de
Matt <mhoffman706@yahoo.com>
2018-12-02 19:50:39 -0700

    test

--- HelloWorldMain.py
+++ HelloWorldMain.py
@@ -1,3 +1,16 @@
 print("hello world")
 print("Hi Matt")
 print("Boo")
+
+from tkinter import *           # Importing the Tkinter (tool box) library
+
+
+def Pressed():  # function
+    print buttons are cool'
+
+
+root = Tk()  # main window
+button = Button(root, text='Print in Console', command=Pressed)
+button.pack(pady=20, padx=20)
+Pressed()
+root.mainloop()
